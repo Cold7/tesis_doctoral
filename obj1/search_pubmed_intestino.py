892414a8a99c5a24191f5fc29f5ddd547eb83e4a
Cold7 <contrerasriquelme.sebastian@gmail.com>
2021-02-09 13:14:24 -0300

    Update search_pubmed_intestino.py

--- obj1/search_pubmed_intestino.py
+++ obj1/search_pubmed_intestino.py
@@ -1,6 +1,6 @@
 from Bio import Entrez
 def search(query):
-	Entrez.email = 'XXXXX@XXXX.XX'
+	Entrez.email = 'scontreras@dlab.cl'
 	handle = Entrez.esearch(db='pubmed',
                             sort='relevance',
                             retmax='20',
@@ -24,33 +24,12 @@ if __name__ == "__main__":
 			"ENSMUSG00000074272":"Ceacam1",
 			"ENSMUSG00000022508":"Bcl6",
 			"ENSMUSG00000039220":"Ppp1r10",
-			"ENSMUSG00000050272":"Dscam",
-			"ENSMUSG00000013419":"Zfp651",
-			"ENSMUSG00000030364":"Clec2h",
-			"ENSMUSG00000040284":"Gzmg",
-			"ENSMUSG00000035441":"Myo1d",
-			"ENSMUSG00000076471":"Trbv14",
-			"ENSMUSG00000013653":"1810065E05Rik",
-			"ENSMUSG00000022263":"Trio",
-			"ENSMUSG00000035775":"Krt20",
-			"ENSMUSG00000076472":"Trbv15",
+			"ENSMUSG00000050272":"Dscam"
 			}
 	print("IBD")
 	for geneID in dict:
 		geneName = dict[geneID]
-		result = search("((IBD) OR (inflammatory bowel disease) OR (Crohn's disease) OR (Crohn disease) OR (ulcerative colitis)) AND ((T cell) OR (Th1) OR (Th2) OR (Th17) OR (Treg)) AND (("+geneName+") OR ("+geneID+"))")
-		id_list = result['IdList']
-		for id in id_list:
-			try:
-				paper = fetch_details(id)
-				print(geneID, geneName,paper["PubmedArticle"][0]["MedlineCitation"]["Article"]["ArticleTitle"])
-			except:
-				pass
-	print("\n")
-	print("Cancer")
-	for geneID in dict:
-		geneName = dict[geneID]
-		result = search("((colorectal cancer) OR (colitis-associated cancer)) AND ((T cell) OR (Th1) OR (Th2) OR (Th17) OR (Treg)) AND (("+geneName+") OR ("+geneID+"))")
+		result = search("((IBD) OR (inflammatory bowel disease) OR (Crohn's disease) OR (Crohn disease) OR (ulcerative colitis)) AND ((CD4 T cell) OR (CD4+ T cell) OR (Th1) OR (Th2) OR (Th17) OR (Th9) OR (T fh) OR (Tfh) or OR (T folicular helper) OR (Treg)) AND (("+geneName+") OR ("+geneID+"))")
 		id_list = result['IdList']
 		for id in id_list:
 			try:
